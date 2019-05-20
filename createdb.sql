3f6bf944dd2af4297b26127bf3f76d3d16394d3d
Kenley Arai <me@kenleyar.ai>
2019-05-20 15:06:47 -0700

    Initial commit
    * Contains SQL commands to create the database
    * Dockerfile for the Postgres database
    * pipfile to install python packages

---
+++ createdb.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS users, leases, tenants, managers, owners, due_leases, pending_leases;
+
+CREATE TABLE users (
+	firstname TEXT,
+	lastname TEXT,
+	email TEXT NOT NULL UNIQUE,
+	PASSWORD TEXT NOT NULL,
+	user_id SERIAL PRIMARY KEY);
+
+CREATE TABLE leases (
+	address TEXT,
+	monthly_rent NUMERIC (5, 2),
+	start_date DATE,
+	end_date DATE,
+	payment_day INT CHECK (payment_day <= 31),
+	lease_id SERIAL PRIMARY KEY);
+
+CREATE TABLE tenants (
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE managers (
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE owners (
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE due_leases (
+    user_id INTEGER,
+    lease_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (lease_id) REFERENCES leases(lease_id)
+);
+
+CREATE TABLE pending_leases (
+    user_id INTEGER,
+    lease_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (lease_id) REFERENCES leases(lease_id)
+);
